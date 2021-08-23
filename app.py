b2e4bff06dec4701bca50134db4c821a82ab4344
huancnttuet <huancnttuet@gmail.com>
2021-08-23 23:32:48 +0700

    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-api = Api(app, version='1.1', title='VNU API',
+api = Api(app, version='1.2', title='VNU TOOL API',
           description='A simple connect daotao.vnu.edu.vn API by Huaan ^^',
           )
 
