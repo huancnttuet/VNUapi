e975ce672cb3a1cbb9bf0bb7956028b3b5742202
huancnttuet <huancnttuet@gmail.com>
2021-08-23 21:46:27 +0700

    add dockerfile

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_restplus import Api, Resource, fields
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='1.0', title='VNU API',
+    description='A simple connect daotao.vnu.edu.vn API',
+)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+ns = api.namespace('crawler', description='CRAWLER operations')
+
+model = api.model('Model', {
+    'name': fields.String,
+    'address': fields.String,
+    'date_updated': fields.DateTime(dt_format='rfc822'),
+})
+
+@ns.route('/todo')
+class Todo(Resource):
+    @ns.marshal_with(model, envelope='resource')
+    def get(self, **kwargs):
+        return "hello"  # Some function that queries the db
+
