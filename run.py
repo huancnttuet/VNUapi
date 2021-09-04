3806f52916a4e46c24fd7d4f7185b50dbebad4d0
huancnttuet <huancnttuet@gmail.com>
2021-09-04 18:24:31 +0700

    hiih

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
-from flask import Flask, request, abort, jsonify, send_from_directory
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, make_response, send_from_directory
+from werkzeug.utils import secure_filename
+
+
 from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
 from api.services import daotao
-import os
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png',
+                      'jpg', 'jpeg', 'gif', 'exe', 'zip', 'rar'}
 
 app = Flask(__name__)
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = ROOT_DIR + UPLOAD_FOLDER
+app.add_url_rule(
+    "/uploads/<name>", endpoint="download_file", build_only=True
+)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app, version='1.2', title='VNU TOOL API',
           description='A simple connect daotao.vnu.edu.vn API by Huaan ^^',
@@ -19,45 +32,38 @@ model = api.model('Model', {
 })
 
 
-UPLOAD_DIRECTORY = "/project/api_uploaded_files"
-
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
-
-
-@ns.route("/files")
-class File(Resource):
-    def get():
-        """Endpoint to list files on the server."""
-        files = []
-        for filename in os.listdir(UPLOAD_DIRECTORY):
-            path = os.path.join(UPLOAD_DIRECTORY, filename)
-            if os.path.isfile(path):
-                files.append(filename)
-        return jsonify(files)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@ns.route("/files/<path:path>")
-class FilePath(Resource):
-    def get(path):
-        """Download a file."""
-        return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+@ns.route('/file', methods=['GET', 'POST'])
+class UploadFile(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('upload.html'), 200, headers)
+
+    def post(self):
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
 
 
-@ns.route("/files/<filename>", methods=["POST"])
-class FileUpload(Resource):
-    def post(filename):
-        """ Upload a file."""
-
-        if "/" in filename:
-            # Return 400 BAD REQUEST
-            abort(400, "no subdirectories allowed")
-
-        with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
-            fp.write(request.data)
-
-        # Return 201 CREATED
-        return "", 201
+@ns.route('/uploads/<string:name>')
+class DownloadFile(Resource):
+    def get(self, name):
+        return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
 @ns.route('/getGPA')
