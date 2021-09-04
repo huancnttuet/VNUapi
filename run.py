7e1dfd7df9b42b16231c506680d8321777596469
huancnttuet <huancnttuet@gmail.com>
2021-09-04 17:28:15 +0700

    a

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify, send_from_directory
 from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
 from api.services import daotao
-
+import os
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -17,6 +17,44 @@ model = api.model('Model', {
     'address': fields.String,
     'date_updated': fields.DateTime(dt_format='rfc822'),
 })
+
+
+UPLOAD_DIRECTORY = "/project/api_uploaded_files"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
+
+@ns.route("/files")
+def list_files():
+    """Endpoint to list files on the server."""
+    files = []
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return jsonify(files)
+
+
+@ns.route("/files/<path:path>")
+def get_file(path):
+    """Download a file."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+
+
+@ns.route("/files/<filename>", methods=["POST"])
+def post_file(filename):
+    """Upload a file."""
+
+    if "/" in filename:
+        # Return 400 BAD REQUEST
+        abort(400, "no subdirectories allowed")
+
+    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
+        fp.write(request.data)
+
+    # Return 201 CREATED
+    return "", 201
 
 
 @ns.route('/getGPA')
